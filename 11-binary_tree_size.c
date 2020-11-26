ab18c853bc349183792ba18e683cab8c6293fc70
fadikoubaa19 <2062@gmail.com>
2020-11-26 21:10:57 +0000

    task11

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,9 +1,9 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_size - size of binary tree
- * @tree: pointer to root node
- * Return: size of tree
+ * binary_tree_size - for holbertonschool
+ * @tree: varr
+ * Return: 0
  */
 
 size_t binary_tree_size(const binary_tree_t *tree)
