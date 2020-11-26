a1430683e77f1bc5b48bc2e6131c295e54f57bc5
fadikoubaa19 <2062@gmail.com>
2020-11-26 21:47:16 +0000

    task15

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_full - full 1 not full 0 
+ * @tree: var
+ * Return: 0 or 1
+ */
+
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+if (!tree)
+return (0);
+if (tree->left == 0 && tree->right == 0)
+return (1);
+if (tree->right && tree->left)
+return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+return (0);
+}
